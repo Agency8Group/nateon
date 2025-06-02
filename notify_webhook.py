8e1d676c7bc389fdaa631bf943199eebb01d17fa
Agency8group <AgencyGroup@naver.com>
2025-06-02 17:33:15 +0900

    오픈~

--- notify_webhook.py
+++ notify_webhook.py
@@ -2,7 +2,7 @@ import requests
 
 webhook_url = "https://teamroom.nate.com/api/webhook/910f895f/3oQ1GiUSb0Vm4NdyZjI8mWYy"
 message = {
-    "text": "✅ In-house-Magazine 페이지가 업데이트 되었습니다.\n\n👉 https://agency8group.github.io/In-house-Magazine/\n(공지사항test)"
+    "text": "✅ In-house-Magazine 페이지가 업데이트 되었습니다.\n\n👉 https://agency8group.github.io/In-house-Magazine/\n(인하우스 매거진 사이트가 오픈하였습니다. 편하게 둘러봐주세요)"
 }
 
 resp = requests.post(webhook_url, json=message)
